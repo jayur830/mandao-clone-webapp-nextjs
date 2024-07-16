895bb4e6069f95827fe485697a954961a6fe5549
jayur830 <jayur830@naver.com>
2024-07-16 23:34:19 +0900

    fix: preview base64 파싱 로직 수정

--- src/app/preview/[base64]/page.tsx
+++ src/app/preview/[base64]/page.tsx
@@ -1,4 +1,5 @@
 export default function Page({ params: { base64 } }: { params: { base64: string } }) {
-  const value = JSON.parse(Buffer.from(base64, 'base64').toString('utf8'));
+  const str = Buffer.from(base64, 'base64').toString('utf8');
+  const value = JSON.parse(str.endsWith('7') ? str.substring(0, str.length - 1) : str);
   return <code style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(value, null, 2)}</code>;
 }
